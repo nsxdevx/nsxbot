404fae37ad588c443dfd1f60846e42e573ed65df
hackerxiao <2945294768@qq.com>
2025-04-18 17:55:04 +0800

    feat:  事件类型分文件定义

--- types/event_msg.go
+++ types/event_msg.go
@@ -178,26 +178,3 @@ type Sender struct {
 	Sex      string `json:"sex"`
 	Age      int    `json:"age"`
 }
-
-type EventNotice struct {
-}
-
-type EventRequest struct {
-}
-
-type LifeMeta struct {
-	SubType string `json:"sub_type"` //enable disable connect
-}
-
-func (e LifeMeta) Type() EventType {
-	return "meta_event:lifecycle"
-}
-
-type HeartMeta struct {
-	Status   Status `json:"status"`
-	Interval int64  `json:"interval"`
-}
-
-func (e HeartMeta) Type() EventType {
-	return "meta_event:heartbeat"
-}
---
+++ types/event_notice.go
@@ -0,0 +1,4 @@
+package types
+
+type EventNotice struct {
+}
