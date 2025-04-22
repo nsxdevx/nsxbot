a8e44ff9ee89a73ea0da15da3187894be4177c47
hackerxiao <2945294768@qq.com>
2025-04-22 16:04:24 +0800

    fix: 修复消息回复方法调用，确保正确传递上下文

--- examples/reply/reply.go
+++ examples/reply/reply.go
@@ -27,7 +27,7 @@ func main() {
 			return
 		}
 		ctx.Log.Info("Private Message", "message", text.Text)
-		msg.Reply(ctx.Replyer, text.Text)
+		msg.Reply(ctx, text.Text)
 		var msgchain types.MeaasgeChain
 		ctx.SendPvtMsg(ctx, adminuin, msgchain.Text("收到回复了吗？").Br().Text("2333333333"))
 	})
